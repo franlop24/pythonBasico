c73607b6d809dc2123e3ff410309fd8790f0bae3
Francisco Lopez <franlopbri@gmail.com>
2022-02-22 14:56:45 -0600

    Se agrega nueva funcion

--- funciones.py
+++ funciones.py
@@ -6,12 +6,18 @@ def tabla_multiplicar(numero):
 def area_circulo(radio):
     print(f"El área de un circulo de radio { radio } es { 3.14 * radio ** 2 } cm2")
 
+def area_cuadrado(lado):
+    print(f"El área de un cuadrado de lado { lado } es { lado * lado } cm2")
+
 def run():
     print("Se imprime la tabla de ultiplicar del 16\n")
     tabla_multiplicar(16)
 
-    print("Calcula un radio 24cm")
+    print("Calcula un radio 24cm\n")
     area_circulo(24)
+
+    print("Calcula un lado 24cm\n")
+    area_cuadrado(100)
 
 
 if __name__ == '__main__':
