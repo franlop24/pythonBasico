042c2ebc9ee343f4e5e3b4cfe9e1e590a68983c4
Francisco Lopez <franlopbri@gmail.com>
2022-03-01 13:31:41 -0600

    se agrega funcion peri

--- funciones.py
+++ funciones.py
@@ -9,12 +9,18 @@ def area_circulo(radio):
 def area_cuadrado(lado):
     print(f"El área de un cuadrado de lado { lado } es { lado * lado } cm2")
 
+def peri_circulo(radio):
+    print(f"El perimetro del circulo de radio {radio} es { 3.14 * (2 * radio) }")
+
 def run():
     print("Se imprime la tabla de ultiplicar del 16\n")
     tabla_multiplicar(16)
 
     print("Calcula un radio 24cm\n")
     area_circulo(24)
+
+    print("Calcula perimetro con radio 24cm\n")
+    peri_circulo(24)
 
     print("Calcula un lado 24cm\n")
     area_cuadrado(100)
